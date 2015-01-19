65fe769b424105a50f0d4496f2e9d80c24d70e78
conates <conates.ktn@gmail.com>
2015-01-19 17:37:32 -0300

    boostrap

--- public/config.php
+++ public/config.php
@@ -11,7 +11,7 @@ $app->configureMode('development', function () use ($app,&$config) {
     ));
 
     define('SITE_URL','http://localhost/instadminer/public/');
-    define('TEMPLATE_URL','http://localhost/instadminer/public/templates_dev');
+    define('TEMPLATE_URL','http://localhost/instadminer/public/templates_dev/');
     
     R::setup('mysql:host=localhost;dbname=instadminer','root','');
     R::freeze( FALSE );
